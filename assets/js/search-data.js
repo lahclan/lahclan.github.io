5f1cddbd6b70b5034d74acafcc72f4da399202f6
lahclan <160426455+lahclan@users.noreply.github.com>
2026-01-27 16:02:30 +0000

    Deploying to gh-pages from @ lahclan/lahclan.github.io@66a03411eeea5cd13eadcab91495cae194af81e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,17 +96,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/post-citation/";
           
         },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/pseudocode/";
-          
-        },
       },{id: "post-a-post-with-code-diff",
         
           title: "a post with code diff",
