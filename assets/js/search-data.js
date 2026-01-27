04e8760db08f0ffc01f72f3232c790a321a463d9
lahclan <160426455+lahclan@users.noreply.github.com>
2026-01-27 19:43:13 +0000

    Deploying to gh-pages from @ lahclan/lahclan.github.io@7dc65d9d9e85c06df24803edda3fc925c81d4cbc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,15 +37,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-about",
+        },{id: "post-favorites",
         
-          title: "about",
+          title: "Favorites",
         
-        description: "",
+        description: "A list of my favorite things",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/";
+            window.location.href = "/blog/2026/favorites/";
           
         },
       },{id: "books-the-godfather",
