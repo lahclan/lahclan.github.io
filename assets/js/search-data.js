51d41c9f9f55e21e8da3d40cba493cb7274b3429
lahclan <160426455+lahclan@users.noreply.github.com>
2026-01-28 13:20:46 +0000

    Deploying to gh-pages from @ lahclan/lahclan.github.io@11a0dbab39522e9185a16005d4189fae1843eaa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,11 +53,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/siddhartha/";
-            },},{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
