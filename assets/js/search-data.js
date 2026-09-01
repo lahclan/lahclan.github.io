d3e7bcfe25b7284c9ed8162a7b274f6f13624b03
lahclan <160426455+lahclan@users.noreply.github.com>
2026-09-01 16:25:20 +0000

    Deploying to gh-pages from @ lahclan/lahclan.github.io@25fc80224e54802627b20813041cda5fae92925f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "None yet :/",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
